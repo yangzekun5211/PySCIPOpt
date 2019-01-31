420bb095aa6dbc4df63e6bd5cdc13028f02c22ce
Matthias Miltenberger <miltenberger@zib.de>
2019-01-31 17:33:41 +0100

    testing around with travis

--- .travis/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    wget http://scip.zib.de/download/release/SCIPOptSuite-$VERSION-Linux.deb
-    sudo apt-get update && sudo apt install -y ./SCIPOptSuite-$VERSION-Linux.deb
-else
-    wget http://scip.zib.de/download/release/SCIPOptSuite-$VERSION-Darwin.dmg
-    sudo hdiutil attach ./SCIPOptSuite-$VERSION-Darwin.dmg
-fi
